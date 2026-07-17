6b17344f4f97ef846927186b8caf4d301c10e4a0
jeisonfifa2026-byte <jeisonfifa2026@gmail.com>
2026-07-17 18:19:13 -0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -13,7 +13,7 @@ export const products: Product[] = [
     colors: [
       { name: 'Azul', hex: '#3b6bb0', images: ['/images/catalog/111azul.png'] },
       { name: 'Gris', hex: '#8a8a8a', images: ['/images/catalog/aaaaa.jpeg'] },
-      { name: 'Negro', hex: '#1c1c1c', images: ['/images/catalog/111.jpeg'] },
+      { name: 'Negro', hex: '#1c1c1c', images: ['/images/catalog/111.png'] },
     ],
     sizes: [
       { name: 'XS', price: 42500 },
