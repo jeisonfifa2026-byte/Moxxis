4ed7d779d31f029906e92990fc329c9126cbea2b
jeisonfifa2026-byte <jeisonfifa2026@gmail.com>
2026-07-22 07:01:57 -0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -12,7 +12,7 @@ export const products: Product[] = [
       'Diseño ergonómico acolchado, equipado con tensores que permiten ajustar el tamaño del artículo. Fabricado con materiales de alta resistencia, suaves y transpirables. Sus bordes redondeados evitan rozaduras en la piel y el pelaje de la mascota. Incluye traílla.',
     colors: [
       { name: 'Azul', hex: '#3b6bb0', images: ['/images/catalog/111azul.png'] },
-      { name: 'Gris', hex: '#8a8a8a', images: ['/images/catalog/girrrs.png'] },
+      { name: 'Gris', hex: '#8a8a8a', images: ['/images/catalog/menorcito1.png'] },
       { name: 'Negro', hex: '#1c1c1c', images: ['/images/catalog/111.png'] },
     ],
     sizes: [
