5ec752f3986c9cf99704b65951a5b1c05f091c61
jeisonfifa2026-byte <jeisonfifa2026@gmail.com>
2026-07-22 07:26:15 -0500

    Updated Catalog.tsx

--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -140,7 +140,7 @@ export default function Catalog() {
               style={{ height: 'clamp(200px, 30vw, 320px)' }}
             >
               <img
-                src="/images/catalog/asdasd.png"
+                src="/images/catalog/asdasd copy.png"
                 alt="Perros MOXX"
                 className="w-full h-full object-cover"
                 style={{ objectPosition: 'center 55%' }}
@@ -162,7 +162,7 @@ export default function Catalog() {
               style={{ height: 'clamp(200px, 30vw, 320px)' }}
             >
               <img
-                src="/images/catalog/asdasd.png"
+                src="/images/catalog/asdasd copy.png"
                 alt="Gatos MOXX"
                 className="w-full h-full object-cover"
                 style={{ objectPosition: 'center 55%' }}
